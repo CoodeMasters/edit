d526b73d927430770343296ffae613dd6495d02e
Hamied-Djalal <hamiedabddjalal9@gmail.com>
2024-11-19 08:17:18 +0100

    add ....

--- resources/lang/dz/new-messages.php
+++ resources/lang/dz/new-messages.php
@@ -3,4 +3,5 @@
 return [
 	"are_you_sure,_want_to_start_auto_translation" => "Are you sure  want to start auto translation",
 	"if_you_encounter_any_issues_during_the_translation_process,_please_contact_your_administrator." => "If you encounter any issues during the translation process  please contact your administrator.",
+	"New_Message" => "New Message",
 ];
